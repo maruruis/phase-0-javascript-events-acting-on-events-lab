e124c8e324db4f664bfc2a70dae74b411a409cd6
Ian Kimani <ianmwng@gmail.com>
2023-09-28 10:37:05 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left -1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    const left = parseInt(dodger.style.left, 10) || 0;
+    const newLeft = left + 1;
+  
+    if (newLeft < 360) { 
+      dodger.style.left = `${newLeft}px`;
+    }
+  }
+  
+
